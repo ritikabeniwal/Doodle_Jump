d4ad527c72b6f4550962fd91c8ef77822d0dd1c5
Ritika_Beniwal_ <ritika.beniwal@sjsu.edu>
2021-10-25 23:43:25 -0700

    Watchdog Final

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -16,7 +16,7 @@
 #include "semphr.h"
 #include "sj2_cli.h"
 #include "task.h"
-#define MAX_SIZE 20
+#define MAX_SIZE 5
 
 #define PRODUCER_WAIT_BITS (1 << 0)
 #define CONSUMER_WAIT_BITS (1 << 1)
@@ -27,7 +27,9 @@
 static SemaphoreHandle_t file_mutex;
 static EventGroupHandle_t event_group;
 static QueueHandle_t accelerator_queue;
-static acceleration__axis_data_s consumer_sensor_value[100];
+static acceleration__axis_data_s consumer_sensor_value[MAX_SIZE];
+static TickType_t time_arr[MAX_SIZE];
+
 static const char *filename = "sensor.txt";
 
 void write_watchdog_error_in_file(int event) {
@@ -58,7 +60,7 @@ void write_watchdog_error_in_file(int event) {
   }
 }
 
-void write_file_using_fatfs_pi(acceleration__axis_data_s *val) {
+void write_file_using_fatfs_pi(acceleration__axis_data_s *val, TickType_t *time_arr) {
 
   FIL file; // File handle
   UINT bytes_written = 0;
@@ -68,7 +70,7 @@ void write_file_using_fatfs_pi(acceleration__axis_data_s *val) {
     char string[256];
     int offset = 0;
     for (int i = 0; i < MAX_SIZE; i++) {
-      sprintf(string + offset, "Time %li, x:%i x:%i x:%i\n", xTaskGetTickCount(), val[i].x, val[i].y, val[i].z);
+      sprintf(string + offset, "Time %li, x:%i y:%i z:%i\n", time_arr[i], val[i].x, val[i].y, val[i].z);
       offset = strlen(string);
     }
 
@@ -106,7 +108,6 @@ void producer_of_accelerator_sensor(void *p) {
     sensor_value.z = result.z / 100;
 
     xQueueSend(accelerator_queue, &sensor_value, 0);
-    //  printf("Sent: %i, %i, %i\n", sensor_value.x, sensor_value.y, sensor_value.z);
 
     i = 0;
     result.x = 0;
@@ -125,13 +126,12 @@ void consumer_of_accelerator_sensor(void *p) {
     static int i = 0;
     if (!xQueueReceive(accelerator_queue, &accelerator_sensor_value, portMAX_DELAY))
       ;
-    //    printf("Received: %i %i %i\n", accelerator_sensor_value.x, accelerator_sensor_value.y,
-    //    accelerator_sensor_value.z);
-    consumer_sensor_value[i] = accelerator_sensor_value;
 
-    if (i == MAX_SIZE) {
+    consumer_sensor_value[i] = accelerator_sensor_value;
+    time_arr[i] = xTaskGetTickCount();
+    if (i == MAX_SIZE - 1) {
       i = 0;
-      //      write_file_using_fatfs_pi(consumer_sensor_value);
+      write_file_using_fatfs_pi(consumer_sensor_value, time_arr);
     }
     i++;
     xEventGroupSetBits(event_group, CONSUMER_WAIT_BITS);
@@ -144,16 +144,13 @@ void watchdog_task(void *params) {
     watch_dog_event_bits =
         xEventGroupWaitBits(event_group, PRODUCER_WAIT_BITS | CONSUMER_WAIT_BITS, pdTRUE, pdTRUE, 200);
     if ((watch_dog_event_bits & (PRODUCER_WAIT_BITS | CONSUMER_WAIT_BITS)) == 0) {
-      // log both of them failed
-      printf("WatchDog fired for both Consumer and Producer Tasks\n");
+
       write_watchdog_error_in_file(WATCHDOG_FIRED_BOTH_TASKS);
     } else if ((watch_dog_event_bits & PRODUCER_WAIT_BITS) == 0) {
-      // log producer hit watchdog
-      printf("WatchDog fired for Producer Task\n");
+
       write_watchdog_error_in_file(WATCHDOG_FIRED_PRODUCER);
     } else if ((watch_dog_event_bits & CONSUMER_WAIT_BITS) == 0) {
-      // log consumer hit watchdog
-      printf("WatchDog fired for Consumer Task\n");
+
       write_watchdog_error_in_file(WATCHDOG_FIRED_CONSUMER);
     } else {
     }
@@ -179,4 +176,3 @@ int main(void) {
   return 0;
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
