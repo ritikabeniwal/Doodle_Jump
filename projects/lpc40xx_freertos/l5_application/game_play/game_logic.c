a50f0a56f9fccff465bd412d6f9135ba67364cf4
sourab gupta <sourabgupta3@gmail.com>
2021-11-26 18:11:09 -0800

    Add level Prints as we move levels

--- projects/lpc40xx_freertos/l5_application/game_play/game_logic.c
+++ projects/lpc40xx_freertos/l5_application/game_play/game_logic.c
@@ -14,6 +14,13 @@
 #define MAX_JUMP 14
 
 static int score = 0;
+int going_up = 1;
+int level = 0;
+int jumper_row, jumper_col;
+bool collision_detected = 0;
+int jump_count = 0;
+joystick_value joystick_data;
+
 void end_game() {
   led_matrix__clear_data_buffer();
   led_matrix_draw_alphabets_print_string("GAME OVER", 15, 8, RED);
@@ -25,11 +32,11 @@ void end_game() {
   }
 }
 
-void get_jumper_position_based_on_joystick_data(joystick_value *joystick_data, int *row, int *col) {
+void get_jumper_position_based_on_joystick_data(joystick_value *data, int *row, int *col) {
 
-  if (joystick_data->s_x < 2000) {
+  if (data->s_x < 2000) {
     *col += 1;
-  } else if (joystick_data->s_x > 2200) {
+  } else if (data->s_x > 2200) {
     *col -= 1;
   } else {
     return;
@@ -41,66 +48,82 @@ void get_jumper_position_based_on_joystick_data(joystick_value *joystick_data, i
   }
 }
 
+void play_level_1() {
+  update_background_screen(collision_detected);
+  if (going_up) {
+    clear_jumper(jumper_row, jumper_col);
+    joystick_data = get_joystick_data();
+    get_jumper_position_based_on_joystick_data(&joystick_data, &jumper_row, &jumper_col);
+    jumper_row -= 1;
+    draw_jumper(jumper_row, jumper_col);
+    jump_count++;
+#ifdef PRINT_DEBUG
+    fprintf(stderr, "jump_count going up %d MAX_JUMP = %d, going up = %d\n", jump_count, MAX_JUMP, going_up);
+#endif
+    if (jump_count == MAX_JUMP) {
+      going_up = 0;
+#ifdef PRINT_DEBUG
+      fprintf(stderr, "going_up = %d\n", going_up);
+#endif
+    }
+
+  } else {
+    clear_jumper(jumper_row, jumper_col);
+    joystick_data = get_joystick_data();
+    get_jumper_position_based_on_joystick_data(&joystick_data, &jumper_row, &jumper_col);
+    jumper_row += 1;
+    draw_jumper(jumper_row, jumper_col);
+    if (detect_collision_background_screen(jumper_row, jumper_col)) {
+      shift_background_screen_down(jumper_row);
+      clear_jumper(jumper_row, jumper_col);
+      jumper_row = get_last_background_screen_row();
+      draw_jumper(jumper_row, jumper_col);
+      going_up = 1;
+      score += 10;
+      if (score == 50) {
+        level = 1;
+      }
+      jump_count = 0;
+      vTaskDelay(100);
+      return;
+    }
+    jump_count--;
+#ifdef PRINT_DEBUG
+    fprintf(stderr, "jump_count going down  %d\n", jump_count);
+#endif
+    if (jump_count == 0) {
+      end_game();
+    }
+  }
+}
+
+void play_level_2() {
+  led_matrix__clear_data_buffer();
+  led_matrix_draw_alphabets_print_string("LEVEL", 15, 15, GREEN);
+  vTaskDelay(2000);
+  led_matrix__clear_data_buffer();
+  level = 0;
+  return;
+}
+void update_score() { led_matrix_draw_alphabets_print_string("SCORE", 1, 2, GREEN); }
+
 void game_play(void *params) {
-  int going_up = 1;
-  int level = 0;
-  int jumper_row, jumper_col;
-  bool collision_detected = 0;
-  int jump_count = 0;
-  joystick_value joystick_data;
   // Initialize Background Screen
+  led_matrix_draw_alphabets_print_string("LEVEL", 15, 15, GREEN);
+  vTaskDelay(2000);
+  led_matrix__clear_data_buffer();
   initialize_background_screen();
   (void)find_initial_jumper_position(&jumper_row, &jumper_col);
   draw_jumper(jumper_row, jumper_col);
   while (1) {
+    update_score();
     vTaskDelay(100);
     switch (level) {
     case 0:
-      update_background_screen(collision_detected);
-      if (going_up) {
-        clear_jumper(jumper_row, jumper_col);
-        joystick_data = get_joystick_data();
-        get_jumper_position_based_on_joystick_data(&joystick_data, &jumper_row, &jumper_col);
-        jumper_row -= 1;
-        draw_jumper(jumper_row, jumper_col);
-        jump_count++;
-#ifdef PRINT_DEBUG
-        fprintf(stderr, "jump_count going up %d MAX_JUMP = %d, going up = %d\n", jump_count, MAX_JUMP, going_up);
-#endif
-        if (jump_count == MAX_JUMP) {
-          going_up = 0;
-#ifdef PRINT_DEBUG
-          fprintf(stderr, "going_up = %d\n", going_up);
-#endif
-        }
-
-      } else {
-        clear_jumper(jumper_row, jumper_col);
-        joystick_data = get_joystick_data();
-        get_jumper_position_based_on_joystick_data(&joystick_data, &jumper_row, &jumper_col);
-        jumper_row += 1;
-        draw_jumper(jumper_row, jumper_col);
-        if (detect_collision_background_screen(jumper_row, jumper_col)) {
-          shift_background_screen_down(jumper_row);
-          clear_jumper(jumper_row, jumper_col);
-          jumper_row = get_last_background_screen_row();
-          draw_jumper(jumper_row, jumper_col);
-          going_up = 1;
-          score++;
-          jump_count = 0;
-          vTaskDelay(100);
-          continue;
-        }
-        jump_count--;
-#ifdef PRINT_DEBUG
-        fprintf(stderr, "jump_count going down  %d\n", jump_count);
-#endif
-        if (jump_count == 0) {
-          end_game();
-        }
-      }
+      play_level_1();
       continue;
     case 1:
+      play_level_2();
       continue;
     default:
       break;
